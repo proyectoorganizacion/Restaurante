0dfffd0d92561fda8b41056bd901e0648b1d1972
Julian Montoya <julian11gm>
2026-09-05 09:53:22 -0500

    modificar plato

--- src/main/java/repositories/CrearPlatoRepository.java
+++ src/main/java/repositories/CrearPlatoRepository.java
@@ -3,6 +3,7 @@ package repositories;
 import Model.CrearPlato;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class CrearPlatoRepository {
     private final List<CrearPlato> listaPlatos = new ArrayList<>();
@@ -13,5 +14,12 @@ public class CrearPlatoRepository {
 
     public List<CrearPlato> obtenerTodos() {
         return listaPlatos;
+    }
+
+// Modificar plato
+    public Optional<CrearPlato> buscarPorId(Long id) {
+        return listaPlatos.stream()
+                .filter(plato -> plato.getId().equals(id))
+                .findFirst();
     }
 }
